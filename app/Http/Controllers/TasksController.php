87ef452de862357e7e156556e6740a5f045799d8
tayu <sakura.cv87@gmail.com>
2021-03-27 15:11:39 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,10 +47,18 @@ class TasksController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+     // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+        ]);
+        
         // タスクを作成
         $task = new Task;
+        $task->status = $request->status; // 追加
         $task->content = $request->content;
         $task->save();
         
@@ -103,9 +111,16 @@ class TasksController extends Controller
      // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required|max:255',
+            ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
+        $task->status = $request->status; //　追加
         $task->content = $request->content;
         $task->save();
         
